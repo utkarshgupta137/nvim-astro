11c7c5482d9d4803ccec80f10169cd1cc9686689
Utkarsh Gupta <utkarshgupta137@gmail.com>
2023-04-01 06:29:37 +0530

    fix highlights after astrotheme update

--- highlights/astrotheme.lua
+++ highlights/astrotheme.lua
@@ -1,31 +1,28 @@
 return function(H) -- a table of overrides/changes to the astrodark theme
   local C = require "astrotheme.palettes.astrodark"
-  H["NoiceCursor"] = { bg = C.white }
+  H["NoiceCursor"] = { bg = C.surface2 }
 
   H["@constant.rust"] = { fg = C.orange }
   H["@keyword.function.rust"] = { fg = C.purple }
   H["@namespace.rust"] = { fg = C.cyan }
-  H["@operator"] = {}
   H["@punctuation.special"] = { fg = C.purple }
-  H["@storageClass.lifetime"] = { fg = C.white }
+  H["@storageClass.lifetime"] = { fg = C.surface2 }
 
   H["@lsp.mod.constant"] = { fg = C.orange }
   H["@lsp.mod.mutable"] = { bold = true, underline = true }
   H["@lsp.type.attributeBracket"] = { fg = C.purple }
+  H["@lsp.type.builtinType"] = { fg = C.blue }
   H["@lsp.type.decorator"] = { fg = C.orange }
   H["@lsp.type.enum"] = { fg = C.cyan }
-  H["@lsp.type.function"] = { fg = C.blue }
   H["@lsp.type.interface"] = { fg = C.blue }
-  H["@lsp.type.keyword"] = { fg = C.purple }
-  H["@lsp.type.lifetime"] = { fg = C.white }
+  H["@lsp.type.lifetime"] = { fg = C.surface2 }
   H["@lsp.type.macro"] = { fg = C.orange, italic = true }
-  H["@lsp.type.method"] = { fg = C.blue }
   H["@lsp.type.namespace"] = { fg = C.cyan }
-  H["@lsp.type.parameter"] = { fg = C.red, italic = true }
-  H["@lsp.type.property"] = { fg = C.red }
+  H["@lsp.type.parameter"] = { italic = true }
   H["@lsp.type.selfKeyword"] = { fg = C.purple, italic = true }
   H["@lsp.type.selfTypeKeyword"] = { fg = C.purple }
-  H["@lsp.type.typeParameter"] = { fg = C.white }
-  H["@lsp.type.variable"] = { fg = C.red }
+  H["@lsp.type.typeParameter"] = { fg = C.surface2 }
+  H["@lsp.typemod.enumMember.defaultLibrary"] = { fg = C.yellow }
+  H["@lsp.typemod.method.defaultLibrary"] = { fg = C.blue }
   return H
 end
