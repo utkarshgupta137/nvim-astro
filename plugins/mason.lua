c47e029b2cc55c01345935baa4cdc0c48dc5217c
Utkarsh Gupta <utkarshgupta137@gmail.com>
2023-05-15 18:08:24 +0530

    mason: add PATH="prepend" for rust-analyzer

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -1,5 +1,11 @@
 -- customize mason plugins
 return {
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      PATH = "prepend",
+    },
+  },
   -- use mason-lspconfig to configure LSP installations
   {
     "williamboman/mason-lspconfig.nvim",
